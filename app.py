6ffd1fa57533adf8b6fdd1029830a252b9bbac6a
Kleirnicole <114151151+Kleirnicole@users.noreply.github.com>
2025-12-02 03:08:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,20 @@
-from flask import Flask, request, jsonify
-from predict_survey import predict
 import os
+from flask import Flask
+from predict_survey import predict
 
 app = Flask(__name__)
 
 @app.route("/predict", methods=["POST"])
 def predict_api():
+    from flask import request, jsonify
     data = request.get_json()
     if not data:
-        return jsonify({"error": "No input JSON provided"}), 400
+        return jsonify({"error": "No JSON provided"}), 400
     try:
-        result = predict(data)
-        return jsonify(result)
+        return jsonify(predict(data))
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render PORT
+    port = int(os.environ.get("PORT", 10000))  # default to 10000 for local testing
     app.run(host="0.0.0.0", port=port)
